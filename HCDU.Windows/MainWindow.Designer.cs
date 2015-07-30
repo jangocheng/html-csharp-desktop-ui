2b900157706bea831f13543d91a53beb9f147ac4
Yuriy Kopylov <iyurij@gmail.com>
2015-07-30 05:39:58 +0500

    Added CefSharp browser control.

--- HCDU.Windows/MainWindow.Designer.cs
+++ HCDU.Windows/MainWindow.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.googleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowserPanel = new System.Windows.Forms.Panel();
+            this.devToolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.googleToolStripMenuItem,
+            this.devToolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1132, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // googleToolStripMenuItem
+            // 
+            this.googleToolStripMenuItem.Name = "googleToolStripMenuItem";
+            this.googleToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.googleToolStripMenuItem.Text = "Google";
+            this.googleToolStripMenuItem.Click += new System.EventHandler(this.MenuActionGoToGoogle);
+            // 
+            // webBrowserPanel
+            // 
+            this.webBrowserPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowserPanel.Location = new System.Drawing.Point(0, 27);
+            this.webBrowserPanel.Name = "webBrowserPanel";
+            this.webBrowserPanel.Size = new System.Drawing.Size(1132, 602);
+            this.webBrowserPanel.TabIndex = 1;
+            // 
+            // devToolsToolStripMenuItem
+            // 
+            this.devToolsToolStripMenuItem.Name = "devToolsToolStripMenuItem";
+            this.devToolsToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.devToolsToolStripMenuItem.Text = "DevTools";
+            this.devToolsToolStripMenuItem.Click += new System.EventHandler(this.MenuActionOpenDevTools);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 629);
+            this.Controls.Add(this.webBrowserPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem googleToolStripMenuItem;
+        private System.Windows.Forms.Panel webBrowserPanel;
+        private System.Windows.Forms.ToolStripMenuItem devToolsToolStripMenuItem;
     }
 }
 
--- HCDU.Windows/MainWindow.cs
+++ HCDU.Windows/MainWindow.cs
@@ -1,12 +1,39 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
+using CefSharp.WinForms;
 
 namespace HCDU.Windows
 {
     public partial class MainWindow : Form
     {
+        private ChromiumWebBrowser webBrowser;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitBrowser();
+        }
+
+        private void InitBrowser()
+        {
+            webBrowser = new ChromiumWebBrowser("about:blank");
+            webBrowser.Name = "webBrowser";
+            webBrowser.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            webBrowser.Location = new Point(0, 0);
+            webBrowser.Size = webBrowserPanel.ClientSize;
+            webBrowser.TabIndex = 1;
+
+            webBrowserPanel.Controls.Add(webBrowser);
+        }
+
+        private void MenuActionGoToGoogle(object sender, System.EventArgs e)
+        {
+            webBrowser.Load("http://www.google.com");
+        }
+
+        private void MenuActionOpenDevTools(object sender, System.EventArgs e)
+        {
+            webBrowser.ShowDevTools();
         }
     }
 }
