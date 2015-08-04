91f8b3a05c767fdf515ff1905fe0eb25d518aea7
Yuriy Kopylov <iyurij@gmail.com>
2015-08-05 02:20:53 +0500

    Implemented Linux wrapper application using Gtk# and webkit-sharp.

---
+++ HCDU.Linux.Gtk/MainWindow.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Gtk;
+using HCDU.API;
+using HCDU.Content;
+using WebKit;
+using HCDU.API.Server;
+using System.Threading;
+
+public partial class MainWindow: Gtk.Window
+{
+	private const string BaseUrl = "http://localhost:8899/";
+	private const string HomePageUrl = BaseUrl + "index.html";
+
+	private WebServer webServer;
+	private WebView webBrowser;
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+
+		//todo: bug, window cannot be resized to smaller size
+		this.Resize (1024, 640);
+
+		ContentPackage contentPackage = new ContentPackage();
+		HcduContent.AppendTo(contentPackage);
+		DebugPages.AppendTo(contentPackage);
+
+		StartServer (contentPackage);
+
+		InitBrowser (contentPackage);
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	void StartServer (ContentPackage contentPackage)
+	{
+		webServer = new WebServer (contentPackage, 8899);
+		Thread thread = new Thread (webServer.Start);
+		thread.IsBackground = true;
+		thread.Start ();
+	}
+
+	void InitBrowser (ContentPackage contentPackage)
+	{
+		webBrowser = new WebView ();
+		webBrowser.Name = "webBrowser";
+
+		vbox1.Add (webBrowser);
+		global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.vbox1 [webBrowser]));
+		w3.Position = 1;
+		this.vbox1.ShowAll();
+
+		webBrowser.LoadUri(HomePageUrl);
+	}
+
+	protected void MenuActionShowHomePage (object sender, EventArgs e)
+	{
+		webBrowser.LoadUri (HomePageUrl);
+	}
+
+	private void MenuActionShowAngularMaterial(object sender, EventArgs e)
+	{
+		webBrowser.LoadUri ("https://material.angularjs.org");
+	}
+
+	private void MenuActionReload(object sender, EventArgs e)
+	{
+		webBrowser.Reload();
+	}
+
+	private void MenuActionShowResources(object sender, EventArgs e)
+	{
+		webBrowser.LoadUri(BaseUrl + "debug/resources.html");
+	}
+}
